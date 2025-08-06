40cd0331aac54a4bf871fed9eca8f8f5f95836be
sfK <sfk.sjm@gmail.com>
2025-08-06 23:16:56 +0900

    Transactional Outbox 모듈 구현

---
+++ common/outbox-message-relay/src/main/java/kuke/board/common/outboxmessagerelay/MessageRelayConstants.java
@@ -0,0 +1,8 @@
+package kuke.board.common.outboxmessagerelay;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
+public class MessageRelayConstants {
+    public static final int SHARD_COUNT = 4; // 임의의 값
+}
