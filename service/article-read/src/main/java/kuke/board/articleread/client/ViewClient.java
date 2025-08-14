03143e19a862eefc60cca6a677b7ed27925954a0
sfK <sfk.sjm@gmail.com>
2025-08-14 21:13:36 +0900

    게시글 조회 최적화 구현 - 조회수 캐시

--- service/article-read/src/main/java/kuke/board/articleread/client/ViewClient.java
+++ service/article-read/src/main/java/kuke/board/articleread/client/ViewClient.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
@@ -20,7 +21,12 @@ public class ViewClient {
         restClient = RestClient.create(likeServiceUrl);
     }
 
+    // 레디스에서 데이터를 조회해본다.
+    // 레디스에 데이터가 없었다면, count 메소드 내부 로직이 호출되면서, viewService로 원본 데이터를 요청한다. 그리고, 레디스에 데이터를 넣고 응답한다.
+    // 레디스에 데이터가 있었다면, 그 데이터를 그대로 바로 반환한다.
+    @Cacheable(key = "#articleId", value = "articleViewCount")
     public long count(Long articleId) {
+        log.info("[ViewClient.count] articleId={}", articleId);
         try {
            return restClient.get()
                     .uri("/v1/article-views/articles/{articleId}/count", articleId)
