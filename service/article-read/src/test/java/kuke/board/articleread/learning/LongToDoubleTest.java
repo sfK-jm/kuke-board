be33e44187ca048e8cc23e57e0763aeacb1fc329
sfK <sfk.sjm@gmail.com>
2025-08-17 21:24:24 +0900

    게시글 목록 최적화 전략 구현 - Long&Double 테스트

---
+++ service/article-read/src/test/java/kuke/board/articleread/learning/LongToDoubleTest.java
@@ -0,0 +1,23 @@
+package kuke.board.articleread.learning;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+public class LongToDoubleTest {
+    @Test
+    void longToDoubleTest() {
+        // long은 64비트로 정수
+        // double은 64비트로 부동소수점
+        long longValue = 111_111_111_111_111_111L;
+        System.out.println("longValue = " + longValue);
+        double doubleValue = longValue;
+        System.out.println("doubleValue = " + new BigDecimal(doubleValue).toString());
+        long longValue2 = (long) doubleValue;
+        System.out.println("longValue2 = " + longValue2);
+
+        // longValue   = 111111111111111111
+        // doubleValue = 111111111111111104
+        // longValue2  = 111111111111111104
+    }
+}
