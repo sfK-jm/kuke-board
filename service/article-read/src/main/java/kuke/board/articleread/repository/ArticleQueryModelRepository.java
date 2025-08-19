c8cb8fdee939c8282f6880b7708e09138719db03
sfK <sfk.sjm@gmail.com>
2025-08-19 22:39:36 +0900

    게시글 목록 최적화 전략 구현 - 서비스 & 컨트롤러

--- service/article-read/src/main/java/kuke/board/articleread/repository/ArticleQueryModelRepository.java
+++ service/article-read/src/main/java/kuke/board/articleread/repository/ArticleQueryModelRepository.java
@@ -6,7 +6,14 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.function.Function;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
 
 @Repository
 @RequiredArgsConstructor
@@ -41,5 +48,13 @@ public class ArticleQueryModelRepository {
 
     private String generateKey(Long articleId) {
         return KEY_FORMAT.formatted(articleId);
+    }
+
+    public Map<Long, ArticleQueryModel> readAll(List<Long> articleIds) {
+        List<String> keyList = articleIds.stream().map(this::generateKey).toList();
+        return redisTemplate.opsForValue().multiGet(keyList).stream()
+                .filter(Objects::nonNull)
+                .map(json -> DataSerializer.deserialize(json, ArticleQueryModel.class))
+                .collect(toMap(ArticleQueryModel::getArticleId, identity()));
     }
 }
