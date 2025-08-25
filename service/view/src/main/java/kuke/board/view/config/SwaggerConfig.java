c0585b9eab2a491df32b712a1d902a8afcae3095
sfK <sfk.sjm@gmail.com>
2025-08-26 00:09:51 +0900

    API문서를 위한 Swagger 추가

---
+++ service/view/src/main/java/kuke/board/view/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package kuke.board.view.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components());
+    }
+
+}
